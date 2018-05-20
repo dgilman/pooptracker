7c77055afd9210afc9a8ce3add90cd2dfb29cf83
David Gilman <davidgilman1@gmail.com>
2018-05-20 11:55:37 -0400

    Cleanups

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ import config
 
 PAGE_SIZE = 1000
 
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(format='%(asctime)s %(name)s:%(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 def omaha_scraper(c):
     l = logging.getLogger("omaha_scraper")
@@ -50,14 +50,15 @@ CREATE TABLE omaha_sewers (
     offset = 0
     feature_cnt = 0
     line_types = {}
-    SEWER_QUERY = "https://gis.dogis.org/arcgis/rest/services/Public_Works/Sewer_Network/MapServer/1/query?where=SWR_TYPE%21%3D1+and+LINE_TYPE%21%3D%27Abandoned%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=OBJECTID%2CSWR_TYPE%2CLINE_TYPE%2CUP_MANHOLE%2CDN_MANHOLE&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset={0}&resultRecordCount={1}&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson"
+    # SWR_TYPE != 1 and LINE_TYPE NOT IN ('Abandoned','Private')
+    SEWER_QUERY = "https://gis.dogis.org/arcgis/rest/services/Public_Works/Sewer_Network/MapServer/1/query?where=SWR_TYPE%21%3D1+and+LINE_TYPE+NOT+IN+%28%27Abandoned%27%2C+%27Private%27%29&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=OBJECTID%2CSWR_TYPE%2CLINE_TYPE%2CUP_MANHOLE%2CDN_MANHOLE&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset={0}&resultRecordCount={1}&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson"
 
     while True:
-        l.info("Fetching features from offset %s", offset)
+        l.debug("Fetching features from offset %s", offset)
         r = requests.get(SEWER_QUERY.format(offset, SEWER_QUERY)).json()
 
         fetch_feature_cnt = len(r["features"])
-        l.info("Fetched %s features", fetch_feature_cnt)
+        l.debug("Fetched %s features", fetch_feature_cnt)
         feature_cnt += fetch_feature_cnt
 
         if fetch_feature_cnt == 0:
@@ -105,32 +106,60 @@ def omaha_calc(c):
     l.setLevel(logging.INFO)
     l.info('Starting omaha_calc')
 
-    c.execute("DROP TABLE IF EXISTS omaha_terminals")
-    c.execute("""
-CREATE TABLE omaha_terminals(
-    terminal text
-)""")
+    omaha_cleanup(c)
 
-    c.execute('SELECT objectid FROM omaha_sewers')
+    c.execute('SELECT objectid FROM omaha_sewers WHERE downstream IS NULL')
     objectids = c.fetchall()
 
     types = {"exact": 0, "fk anomaly": 0, "geom lookup": 0, "terminal": 0}
     [omaha_calc_sewer(l, c, objectid[0], types) for objectid in objectids]
+    omaha_post_cleanup(c)
     l.info("Done linking sewers.")
 
-#    c.execute("""
-#DELETE FROM omaha_sewers
-#WHERE objectid NOT IN (
-#    SELECT downstream FROM omaha_sewers
-#
-#    UNION
-#
-#    SELECT objectid FROM omaha_sewers WHERE downstream IS NOT NULL
-#)
-#""")
+    # Delete sewers that aren't a downstream and don't have a downstream
+    # aka total orphans not part of any system
+    c.execute("""
+DELETE FROM omaha_sewers
+WHERE objectid IN (
+    SELECT omaha_sewers.objectid
+    FROM omaha_sewers
+    LEFT JOIN (
+        SELECT downstream as objectid FROM omaha_sewers
+        UNION
+        SELECT objectid FROM omaha_sewers WHERE downstream IS NOT NULL
+    ) inn
+        ON omaha_sewers.objectid = inn.objectid
+    WHERE inn.objectid IS NULL
+)
+RETURNING *
+""")
+    l.info("Deleted {0} orphaned sewers.".format(c.rowcount))
 
     l.info('omaha_calc finished. Exact hits: {0}, Multiple exact hits: {1}, Downstreams determined with geo queries: {2}, terminal sewers: {3}'.format(
         types["exact"], types["fk anomaly"], types["geom lookup"], types["terminal"]))
+
+def omaha_cleanup(c):
+    # Downtown combined sewer overflows that aren't particularly helpful
+    #c.execute("DELETE FROM omaha_sewers WHERE objectid IN (81235, 81236)")
+    c.execute("DELETE FROM omaha_sewers WHERE objectid IN (22240, 28775)")
+    c.execute("UPDATE omaha_sewers SET downstream = 62690 WHERE objectid IN (81235, 576370)")
+    c.execute("DELETE FROM omaha_sewers WHERE objectid IN (20466, 81232)")
+    c.execute("DELETE FROM omaha_sewers WHERE objectid IN (4785, 75396, 62286, 62306, 84345, 56053, 96442, 494438, 97695)")
+    # A loop by Creighton
+    c.execute("DELETE FROM omaha_sewers WHERE objectid = 90942")
+    # The north end of Minne Lusa Blvd
+    c.execute("UPDATE omaha_sewers SET downstream = 171306 WHERE objectid = 46415")
+    # Monroe and 17th
+    c.execute("UPDATE omaha_sewers SET downstream = 60042 WHERE objectid = 3755")
+    c.execute("UPDATE omaha_sewers SET downstream = 47814 WHERE objectid IN (60042, 67448, 72717)")
+    # 144th and Giles
+    c.execute("UPDATE omaha_sewers SET downstream = 81703 WHERE objectid IN (100668, 100526)")
+    # Missing pipe near 204th and F?
+    c.execute("UPDATE omaha_sewers SET downstream = 39602 WHERE objectid = 96644")
+
+def omaha_post_cleanup(c):
+    # Fix up the south omaha WTP
+    c.execute("UPDATE omaha_sewers SET downstream = NULL WHERE objectid IN (62690, 634383, 634376)")
 
 def omaha_calc_sewer(l, c, objectid, types):
     # We have this hacky stuff because we need to do tiny distance searches on the last segment of every sewer.
@@ -176,9 +205,6 @@ LIMIT 1
 """, (objectid, objectid, objectid))
         downstream_geom_query = c.fetchall()
         if len(downstream_geom_query) == 0:
-            #l.info('No downstreams for objectid {0}, adding to terminals table'.format(objectid))
-            # XXX make this an integer again now that sewers has points saved
-            #c.execute("INSERT INTO omaha_terminals (terminal) VALUES (%s)", (last_point_geojson,))
             types["terminal"] += 1
         elif len(downstream_geom_query) == 1:
             c.execute("UPDATE omaha_sewers SET downstream = %s WHERE objectid = %s", (downstream_geom_query[0][0], objectid))
